483f35d33fc1a0d5c1d3e7215b11a4a3e3a44676
Om12ek99 <omar.elkamili@gmail.com>
2024-05-02 18:55:55 +0200

    add: mixin flex created

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,18 @@
     export default {
         data (){
             return {
-
+                navArray: [
+                    "CHARACTERS",
+                    "COMICS",
+                    "MOVIES",
+                    "TV",
+                    "GAMES",
+                    "COLLECTIBLES",
+                    "VIDEOS",
+                    "FANS",
+                    "NEWS",
+                    "SHOP"
+                ]
             }
         }
     }
@@ -14,7 +25,11 @@
             <img src="../assets/img/dc-logo.png" alt="">
         </div>
         <div class="navbar">
-
+            <ul>
+                <li v-for = "(CurListItem, index) in navArray">
+                    {{CurListItem}}
+                </li>
+            </ul>
         </div>
     </header>
 </template>
