e315d1697cebafae05cab7a073b3680804093e40
Om12ek99 <omar.elkamili@gmail.com>
2024-05-03 22:31:33 +0200

    add: array of images

--- src/components/AppIcons.vue
+++ src/components/AppIcons.vue
@@ -2,9 +2,28 @@
     export default{
         data(){
             return{
-                iconArray: [
-                    
-                ]
+                image:[
+        {
+          image: "buy-comics-digital-comics.png",
+          title: "DIGITAL COMICS",
+        },
+        {
+          image: "buy-comics-merchandise.png",
+          title: "DC MERCHANDISE",
+        },
+        {
+          image: "buy-comics-subscriptions.png",
+          title: "SUBSCRIPTION",
+        },
+        {
+          image: "buy-comics-shop-locator.png",
+          title: "COMIC SHOP LOCATOR",
+        },
+        {
+          image: "buy-dc-power-visa.svg",
+          title: "DC POWER VISA",
+        },
+          ]
             }
         }
     }
