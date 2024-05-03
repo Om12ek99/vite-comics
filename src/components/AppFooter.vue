8c6f779826edfbce5f0a1b97297bac08d0ffcfba
Om12ek99 <omar.elkamili@gmail.com>
2024-05-03 23:44:33 +0200

    Add: links Array

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,141 @@
+<script>
+    export default{
+        data(){
+            return {
+                dcComicsArray: [
+                    {
+                        title:"CHARACTERS",
+                        isActive: false,
+                    },
+                    {
+                        title:"COMICS",
+                        isActive: false,
+                    },
+                    {
+                        title:"MOVIES",
+                        isActive: false,
+                    },
+                    {
+                        title:"TV",
+                        isActive: false,
+                    },
+                    {
+                        title:"GAMES",
+                        isActive: true,
+                    },
+                    {
+                        title:"COLLECTIBLES",
+                        isActive: false,
+                    },
+                    {
+                        title:"FANS",
+                        isActive: false,
+                    },
+                    {
+                        title:"NEWS",
+                        isActive: false,
+                    },
+                    {
+                        title:"SHOP",
+                        isActive: false,
+                    },                   
+                    
+                    
+                ],
+                shopArray: [
+                    {
+                        title:"Shop DC",
+                        isActive: false,
+                    },  
+                    {
+                        title:"Shop DC collectibles",
+                        isActive: false,
+                    },  
+                    
+                ],
+                dcArray:[
+                    {
+                        title:"Terms of use",
+                        isActive: false,
+                    },  
+                    {
+                        title:"Privacy policy (New)",
+                        isActive: false,
+                    },  
+                    {
+                        title:"Ad Choices",
+                        isActive: false,
+                    },  
+                    {
+                        title:"Advertising",
+                        isActive: false,
+                    },  
+                    {
+                        title:"Jobs",
+                        isActive: false,
+                    }, 
+                    {
+                        title:"Subscription",
+                        isActive: false,
+                    },   
+                    {
+                        title:"Talent Workshop",
+                        isActive: false,
+                    },  
+                    {
+                        title:"CPSC Certificate",
+                        isActive: false,
+                    },  
+                    {
+                        title:"Ratings",
+                        isActive: false,
+                    },  
+                    {
+                        title:"Shop Help",
+                        isActive: false,
+                    },  
+                    {
+                        title:"Contact Us",
+                        isActive: false,
+                    },  
+                ],
+                sitesArray:[
+                    {
+                        title:"DC",
+                        isActive: false,
+                    },  
+                    {
+                        title:"MAD magazine",
+                        isActive: false,
+                    },  
+                    {
+                        title:"DC Kids",
+                        isActive: false,
+                    },  
+                    {
+                        title:"DC Universe",
+                        isActive: false,
+                    },  
+                    {
+                        title:"Dc Power Visa",
+                        isActive: false,
+                    },  
+                ]
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="container">
+        <div class="links">
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
