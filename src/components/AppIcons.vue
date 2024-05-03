44531930085c115b15f612a5df0451ad72837944
Om12ek99 <omar.elkamili@gmail.com>
2024-05-03 18:27:31 +0200

    add:updated variables.scss with colors

---
+++ src/components/AppIcons.vue
@@ -0,0 +1,18 @@
+<script>
+    export default{
+        data(){
+            return{
+                iconArray: [
+                    
+                ]
+            }
+        }
+    }
+</script>
+
+<template>
+</template>
+
+<style scoped lang="scss">
+
+</style>
