bc554c705a82622d40a444c5772ce529448869cf
Om12ek99 <omar.elkamili@gmail.com>
2024-05-05 12:31:03 +0200

    add: successfully used propsto display first image and resolved a mistake of putting props in data()

---
+++ src/components/SeriesCard.vue
@@ -0,0 +1,29 @@
+<script>
+    export default {
+                props: {
+                    cardThumb: String,
+                    cardPrice: String,
+                    cardSeries: String,
+                    cardType: String,
+
+                },
+        }
+</script>
+
+<template>
+    <div class="card">
+        <img :src= "cardThumb"
+             :alt= "cardSeries">
+        <h4> {{cardSeries}} </h4>
+    </div>
+</template>
+<style scoped lang="scss">
+    .card {
+        img{
+            width: 80px;
+        }
+        h4{
+            color: aliceblue;
+        }
+    }
+</style>
